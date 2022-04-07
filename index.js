6e145028ec3936ad1cec2a2df55a692e81f10cbb
alakotko <ejakaa@gmail.com>
2022-04-07 15:47:23 +0400

    prettify everything

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const AllureHermioneReporter = require('./lib/hermione-reporter')
-const updateRefsCheck = require('./lib/updateRefsCheck')
+const AllureHermioneReporter = require('./lib/hermione-reporter');
+const updateRefsCheck = require('./lib/updateRefsCheck');
 
 module.exports = function (hermione, opts) {
-  // Check updateRefs argvs
-  const updateRefs = updateRefsCheck(process.argv)
+	// Check updateRefs argvs
+	const updateRefs = updateRefsCheck(process.argv);
 
-  if (!opts.enabled || updateRefs) {
-    return
-  }
+	if (!opts.enabled || updateRefs) {
+		return;
+	}
 
-  const allureConfig = {
-    resultsDir: opts?.targetDir || 'allure-results',
-    ...opts.reporterOptions
-  }
+	const allureConfig = {
+		resultsDir: opts?.targetDir || 'allure-results',
+		...opts.reporterOptions,
+	};
 
-  const allureReporter = new AllureHermioneReporter(allureConfig)
+	const allureReporter = new AllureHermioneReporter(allureConfig);
 
-  hermione.on(hermione.events.SUITE_BEGIN, function (suite) {
-    allureReporter.startSuite(suite.fullTitle())
-  })
+	hermione.on(hermione.events.SUITE_BEGIN, function (suite) {
+		allureReporter.startSuite(suite.fullTitle());
+	});
 
-  hermione.on(hermione.events.SUITE_END, function (suite) {
-    allureReporter.endSuite(suite)
-  })
+	hermione.on(hermione.events.SUITE_END, function (suite) {
+		allureReporter.endSuite(suite);
+	});
 
-  hermione.on(hermione.events.TEST_BEGIN, function (test) {
-    allureReporter.startCase(test)
-  })
+	hermione.on(hermione.events.TEST_BEGIN, function (test) {
+		allureReporter.startCase(test);
+	});
 
-  hermione.on(hermione.events.TEST_PASS, function (test) {
-    allureReporter.passTestCase(test)
-  })
+	hermione.on(hermione.events.TEST_PASS, function (test) {
+		allureReporter.passTestCase(test);
+	});
 
-  hermione.on(hermione.events.TEST_FAIL, function (test) {
-    allureReporter.failTestCase(test, test.err)
-  })
+	hermione.on(hermione.events.TEST_FAIL, function (test) {
+		allureReporter.failTestCase(test, test.err);
+	});
 
-  hermione.on(hermione.events.TEST_PENDING, function (test) {
-    allureReporter.pendingTestCase(test)
-  })
-}
+	hermione.on(hermione.events.TEST_PENDING, function (test) {
+		allureReporter.pendingTestCase(test);
+	});
+};
